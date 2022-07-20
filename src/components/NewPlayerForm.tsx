9b734729204f67a5deaafd2dd19ac715ad914461
Linus Karlsson <linus.karlsson@tetrapak.com>
2022-07-20 21:51:01 +0200

    add dynamic new players

--- src/components/NewPlayerForm.tsx
+++ src/components/NewPlayerForm.tsx
@@ -2,32 +2,24 @@ import { Field, Form, Formik } from "formik"
 
 type NewGameFormProps = {
     playerPlaceholder: number;
+    handleAddPlayer: () => void;
 }
 
-export function NewGameForm({ playerPlaceholder }: NewGameFormProps) {
+export function NewPlayerForm({ playerPlaceholder, handleAddPlayer }: NewGameFormProps) {
+
+    const addPlayer = () => {
+        handleAddPlayer()
+    }
+
     return (
-        <Formik
-            initialValues={{
-                player: '',
-            }}
-            onSubmit={(values) => {
-                alert(JSON.stringify(values, null, 2))
-            }}
-        >
-            <Form className="relative">
-                <label className="sr-only" htmlFor="player"> Player </label>
-                <Field
-                    className="w-full py-4 pl-3 pr-16 text-sm border-2 border-gray-200 rounded-lg"
-                    id='player'
-                    name='player'
-                    placeholder={`Player ${playerPlaceholder}`}
-                />
-                <button className="absolute p-2 text-white -translate-y-1/2 bg-purple-600 rounded-full top-1/2 right-4" type="button">
-                    <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-                    </svg>
-                </button>
-            </Form>
-        </Formik>
+        <div className="relative">
+            <label className="sr-only" htmlFor={`player-${playerPlaceholder}`}>{` Player `}</label>
+            <Field
+                className="w-full py-4 pl-3 pr-16 text-sm border-2 border-gray-200 rounded-lg"
+                id={`player-${playerPlaceholder}`}
+                name={`player-${playerPlaceholder}`}
+                placeholder={`Player ${playerPlaceholder}`}
+            />
+        </div>
     )
 }
--- src/components/newgame.tsx
+++ src/components/newgame.tsx
@@ -1,12 +1,17 @@
 import { Dialog } from "@headlessui/react";
-
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import { useState } from "react";
-import { NewGameForm } from "./NewPlayerForm";
+import { NewPlayerForm } from "./NewPlayerForm";
+import { PlusCircleIcon } from "@heroicons/react/solid";
 
 export function NewGame() {
 
-    const [players, setPlayers] = useState<string[]>([""]);
+    const [players, setPlayers] = useState<string[]>(["player-1"]);
+
+    const handleAddPlayer = () => {
+        setPlayers([...players, `player-${players.length + 1}`])
+    }
+
     return (
         <>
             <Dialog.Title
@@ -17,18 +22,37 @@ export function NewGame() {
             </Dialog.Title>
             <div className="mt-2">
                 <h4>Add players</h4>
-                <p className="flex flex-col mt-2 space-y-2 text-sm text-gray-500">
-                    {
-                        players.map((player) => {
-                            return (
-                                <NewGameForm
-                                    key={players.indexOf(player) + 1}
-                                    playerPlaceholder={players.indexOf(player) + 1}
-                                />
-                            )
-                        })
-                    }
+                <p className="flex flex-col text-sm text-gray-500">
+
+                    <Formik
+                        initialValues={{
+                            player: '',
+                        }}
+                        onSubmit={(values) => {
+                            alert(JSON.stringify(values, null, 2))
+                        }}
+                    >
+                        <Form className="mt-2 space-y-2 ">
+                            {
+                                players.map((player) => {
+                                    return (
+                                        <NewPlayerForm
+                                            key={players.indexOf(player) + 1}
+                                            playerPlaceholder={players.indexOf(player) + 1}
+                                            handleAddPlayer={handleAddPlayer}
+                                        />
+                                    )
+                                })
+                            }
+                        </Form>
+                    </Formik>
                 </p>
+                <button
+                    className="w-10 h-10 mt-4 text-purple-600 rounded-full"
+                    type="button"
+                    onClick={() => handleAddPlayer()}>
+                    <PlusCircleIcon />
+                </button>
             </div>
         </>
     )
